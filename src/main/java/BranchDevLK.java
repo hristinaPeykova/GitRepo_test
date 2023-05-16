5823e310b3b0ca7216ffd3efe05b0a03be38a677
Kristina Peykova <hristina.peykova@gmail.coma>
2023-05-16 13:15:42 +0300

    commit in DevB

--- src/main/java/BranchDevLK.java
+++ src/main/java/BranchDevLK.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class BranchDevLK {
+public class BranchDevLK {
+    public static void main(String[] args) {
+        System.out.println(" This is developed in the devLocalBranch");
+    }
 }
