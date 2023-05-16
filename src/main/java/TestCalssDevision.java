52eafb064cb2fe39eb3b052df717dd14e1699219
Kristina Peykova <hristina.peikova@effortel.com>
2023-05-16 11:47:51 +0300

    first commit

---
+++ src/main/java/TestCalssDevision.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class TestCalssDevision
+{
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter an integer");
+        int myint = keyboard.nextInt();
+        System.out.println("Got integer  = " + myint);
+        System.out.println(System.getProperty("user.dir"));
+
+
+
+    }
+}
